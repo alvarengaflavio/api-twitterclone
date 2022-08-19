8b09aad24d06afbabcda3edf5b8e168b0bb7cc84
alvalenda <banysan@msn.com>
2022-08-19 07:25:05 -0300

    +deleteUser +createUser +findAllUser

--- src/users/user.controller.js
+++ src/users/user.controller.js
@@ -1,11 +1,50 @@
-export const findAllUserController = (req, res) => {
-  res.send({ message: 'Create user OK!' });
+import * as userService from './user.service.js';
+
+export const findAllUserController = async (req, res) => {
+  try {
+    const users = await userService.findAllUserService();
+    res.status(200).json(users);
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
 };
 
-export const createUserController = (req, res) => {
-  res.send({ message: 'Find all users OK!' });
+export const createUserController = async (req, res) => {
+  try {
+    const createUser = { ...req.body };
+    if (
+      !createUser.username ||
+      !createUser.name ||
+      !createUser.email ||
+      !createUser.password
+    ) {
+      throw new Error(
+        'Missing fields! Required: username, name, email, password and avatar.',
+      );
+    }
+
+    const findUser = await userService.findByEmailUserService(createUser.email);
+    if (findUser) {
+      throw new Error('User already exists!');
+    }
+
+    const newUser = await userService.createUserService(createUser);
+    delete newUser.password;
+    res.status(201).send(newUser);
+  } catch (err) {
+    return res.status(400).send({ message: err.message });
+  }
 };
 
-export const deleteUserController = (req, res) => {
-  res.send({ message: 'Delete user OK!' });
+export const deleteUserController = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleteUser = await userService.deleteUserService(id);
+    if (!deleteUser) {
+      throw new Error('User not found!');
+    }
+    res.status(200).send(deleteUser);
+  } catch (err) {
+    return res.status(400).send({ message: err.message });
+  }
 };
--- src/users/user.service.js
+++ src/users/user.service.js
@@ -1,7 +1,35 @@
-import * as userService from './user.service';
+import { User } from './user.js';
 
-export const findAllUserService = (req, res) => {};
+export const findAllUserService = async () => {
+  try {
+    const users = await User.find();
+    return users;
+  } catch (err) {
+    throw new Error('Error finding all users');
+  }
+};
 
-export const createUserService = (req, res) => {};
+export const createUserService = async createUser => {
+  try {
+    const user = new User({ ...createUser });
+    const newUser = await User.create(user);
+    return newUser.toJSON();
+  } catch (err) {
+    throw new Error('Error creating user');
+  }
+};
 
-export const deleteUserService = (req, res) => {};
+export const deleteUserService = async id => {
+  try {
+    const deletedUser = await User.findByIdAndDelete(id);
+    return deletedUser;
+  } catch (err) {
+    throw new Error('Error deleting user');
+  }
+};
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+export const findByEmailUserService = async email => {
+  const user = await User.findOne({ email });
+  return user;
+};
